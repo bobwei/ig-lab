a4d7fe706cce84f2750c710b54044d3a47aa0b43
Bob Wei <bob.wei@linecorp.com>
2019-11-21 16:44:11 +0800

    Update filename

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ import setupRequest from './functions/setupRequest';
 
 async function fn() {
   setupRequest();
-  if (!fs.existsSync(path.resolve('./tmp/following.json'))) {
+  if (!fs.existsSync(path.resolve('./tmp/followings.json'))) {
     const mapper = R.pipe(
       R.pathOr([], ['data', 'user', 'edge_follow', 'edges']),
       R.map(R.prop('node')),
     );
     await getFollowings({ userId: '110379', recursive: true })
-      .then(mapper())
-      .then(writeToFile(path.resolve('./tmp/following.json')))
+      .then(mapper)
+      .then(writeToFile(path.resolve('./tmp/followings.json')))
       .catch(console.log);
   }
 }
