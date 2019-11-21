fcb33a35a7ed2c45b548e4630340748d03ceb547
Bob Wei <bob.wei@linecorp.com>
2019-11-21 16:18:20 +0800

    Init

---
+++ src/apis/getFollowings/index.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import * as R from 'ramda';
+
+const fn = async (props = {}, output = {}) => {
+  const { userId, first = 12, after } = props;
+  const variables = { id: userId, first, after };
+  if (props.hasNext === false) {
+    return output;
+  }
+  return axios
+    .get(getUrl(variables))
+    .then((res) => res.data)
+    .then(R.mergeDeepWithKey((k, l, r) => (k === 'edges' ? R.concat(l, r) : r), output))
+    .then((results) => {
+      if (!props.recursive) {
+        return results;
+      }
+      const { end_cursor: cursor, has_next_page: hasNext } = getPageInfo()(results);
+      return fn({ ...props, after: cursor, hasNext }, results);
+    });
+};
+
+export default fn;
+
+function getPageInfo() {
+  return R.path(['data', 'user', 'edge_follow', 'page_info']);
+}
+
+function getUrl(variables) {
+  // prettier-ignore
+  return `https://www.instagram.com/graphql/query/?query_hash=d04b0a864b4b54837c0d870b0e77e076&variables=${JSON.stringify(variables)}`;
+}
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import 'dotenv/config';
+import * as R from 'ramda';
+import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+
+import getFollowings from './apis/getFollowings';
+
+async function fn() {
+  setupRequest();
+  getFollowings({ userId: '110379' })
+    .then(mapper())
+    .then(writeToFile(path.resolve('./tmp/following.json')))
+    .catch(console.log);
+}
+
+function setupRequest() {
+  const { COOKIE } = R.evolve({ COOKIE: (s) => Buffer.from(s, 'base64') })(process.env);
+  axios.interceptors.request.use((config) => ({
+    ...config,
+    headers: {
+      ...config.headers,
+      Cookie: COOKIE,
+    },
+  }));
+}
+
+function mapper() {
+  return R.pipe(R.pathOr([], ['data', 'user', 'edge_follow', 'edges']), R.map(R.prop('node')));
+}
+
+function writeToFile(outputPath) {
+  return (content) => {
+    fs.writeFileSync(outputPath, JSON.stringify(content, null, 2));
+    return content;
+  };
+}
+
+if (require.main === module) {
+  fn();
+}
