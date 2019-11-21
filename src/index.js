9682bac8422d4294e59e5f1b49b764735b0c53b4
Bob Wei <bob.wei@linecorp.com>
2019-11-21 16:38:34 +0800

    Add file exists check

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import * as R from 'ramda';
+import fs from 'fs';
 import path from 'path';
 
 import getFollowings from './apis/getFollowings';
@@ -8,14 +9,16 @@ import setupRequest from './functions/setupRequest';
 
 async function fn() {
   setupRequest();
-  getFollowings({ userId: '110379', recursive: true })
-    .then(mapper())
-    .then(writeToFile(path.resolve('./tmp/following.json')))
-    .catch(console.log);
-}
-
-function mapper() {
-  return R.pipe(R.pathOr([], ['data', 'user', 'edge_follow', 'edges']), R.map(R.prop('node')));
+  if (!fs.existsSync(path.resolve('./tmp/following.json'))) {
+    const mapper = R.pipe(
+      R.pathOr([], ['data', 'user', 'edge_follow', 'edges']),
+      R.map(R.prop('node')),
+    );
+    await getFollowings({ userId: '110379', recursive: true })
+      .then(mapper())
+      .then(writeToFile(path.resolve('./tmp/following.json')))
+      .catch(console.log);
+  }
 }
 
 if (require.main === module) {
