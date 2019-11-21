2f20bef2aac9d67f5ac78c8e185c9fb9704b14b8
Bob Wei <bob.wei@linecorp.com>
2019-11-22 00:47:15 +0800

    Be able to get posts

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import fs from 'fs';
 import path from 'path';
 
 import getFollowings from './apis/getFollowings';
+import getOwnerTimeline from './apis/getOwnerTimeline';
 import writeToFile from './functions/writeToFile';
 import setupRequest from './functions/setupRequest';
+import createTaskQueue from './utilities/task-queue';
 
 async function fn() {
   setupRequest();
@@ -18,6 +20,36 @@ async function fn() {
       .then(mapper)
       .then(writeToFile(path.resolve('./tmp/followings.json')))
       .catch(console.log);
+  }
+  if (!fs.existsSync(path.resolve('./tmp/posts.json'))) {
+    await new Promise((resolve) => {
+      const followings = JSON.parse(fs.readFileSync('./tmp/followings.json')).slice(0, 50);
+      // prettier-ignore
+      const props = [
+        'id', 'display_url', 'edge_media_to_caption', 'shortcode', 'edge_media_preview_like',
+        'owner', 'location'
+      ]
+      const mapper = R.pipe(
+        R.pathOr([], ['data', 'user', 'edge_owner_to_timeline_media', 'edges']),
+        R.map(R.prop('node')),
+        R.reject(R.propEq('location', null)),
+        R.map(R.pick(props)),
+      );
+      const arr = [];
+      const queue = createTaskQueue({
+        fn: R.pipeP(getOwnerTimeline, mapper),
+        onData: (arg, res) => {
+          arr.push(...res);
+          writeToFile(path.resolve('./tmp/posts.json'))(arr);
+          console.log(queue.status);
+          if (queue.isComplete) {
+            console.log('arr.length =', arr.length);
+            resolve();
+          }
+        },
+      });
+      followings.forEach((arg) => queue.push(arg));
+    });
   }
 }
 
