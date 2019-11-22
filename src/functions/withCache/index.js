b8ea82e345b4dd8155193c37045f7e571b449ffb
Bob Wei <bob.wei@linecorp.com>
2019-11-22 17:18:21 +0800

    Update cache format

--- src/functions/withCache/index.js
+++ src/functions/withCache/index.js
@@ -2,11 +2,18 @@ function withCache(storage, key, fn) {
   return async (...args) => {
     const cache = await storage.getItem(key);
     if (cache) {
-      return JSON.parse(cache);
+      return JSON.parse(cache).data;
     }
     const result = await fn(...args);
-    await storage.setItem(key, JSON.stringify(result, null, 2));
+    await storage.setItem(key, JSON.stringify(createRes(result), null, 2));
     return result;
+  };
+}
+
+function createRes(data) {
+  return {
+    data,
+    createdAt: new Date().toISOString(),
   };
 }
 
