09aabd3abb8fd2d830a30c849fa48dd713cf6853
Bob Wei <bob.wei@linecorp.com>
2019-11-22 17:13:47 +0800

    Rm unused

--- src/functions/writeToFile/index.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import * as R from 'ramda';
-
-const writeToFile = (outputPath, content) => {
-  fs.writeFileSync(outputPath, JSON.stringify(content, null, 2));
-  return content;
-};
-
-export default R.curry(writeToFile);
